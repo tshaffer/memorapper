3ba85d88be8b3e5732ea1d402479aa573118af6c
Ted Shaffer <shaffer.family@gmail.com>
2024-11-26 05:45:26 -0800

    output MONGODB_URI to console.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -37,6 +37,8 @@ app.use('/api', routes);
 // });
 
 // Connect to MongoDB
+console.log('MONGODB_URI:', process.env.MONGODB_URI);
+
 mongoose.connect(process.env.MONGODB_URI || '')
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Error connecting to MongoDB:', error));
