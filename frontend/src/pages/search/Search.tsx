766b900c48ac4116818ad271ba747189e67ee462
Ted Shaffer <shaffer.family@gmail.com>
2024-12-06 09:52:06 -0800

    fewer diagnostics

--- frontend/src/pages/search/Search.tsx
+++ frontend/src/pages/search/Search.tsx
@@ -23,29 +23,24 @@ const ResizableContainer: React.FC<ResizableContainerProps> = ({ initialTopDivHe
     console.log('deltaY', ui.y);
     // console.log('sensitivityFactor', sensitivityFactor);
 
-    const mathMax = Math.abs(ui.y * sensitivityFactor);
-    const secondPart = containerHeight - handleRef.current!.offsetHeight;
-    console.log('mathMax', mathMax);
-    console.log('secondPart', secondPart);
-    const denominator = containerHeight * 100;
-    console.log('denominator', denominator);
+    // const mathMax = Math.abs(ui.y * sensitivityFactor);
+    // const secondPart = containerHeight - handleRef.current!.offsetHeight;
+    // console.log('mathMax', mathMax);
+    // console.log('secondPart', secondPart);
+    // const denominator = containerHeight * 100;
+    // console.log('denominator', denominator);
 
-    const numerator = Math.min(mathMax, secondPart);
-    console.log('mathMin', numerator);
+    // const numerator = Math.min(mathMax, secondPart);
+    // console.log('mathMin', numerator);
 
-    const finalResult = numerator / denominator;
-    console.log('finalResult', finalResult);
+    // const finalResult = numerator / denominator;
+    // console.log('finalResult', finalResult);
 
-    // const newTopDivHeight = `${mathMin} / containerHeight * 100}%`;
     const newTopDivHeight = `${Math.min(Math.max(0, ui.y * sensitivityFactor), containerHeight - handleRef.current!.offsetHeight)} / containerHeight * 100}%`;
-    // console.log('newTopDivHeight', newTopDivHeight);
     setTopDivHeight(newTopDivHeight);
-
-    console.log('parseFloat(topDivHeight)', parseFloat(newTopDivHeight));
-
   };
 
-  console.log('topDivHeight', topDivHeight);
+  // console.log('topDivHeight', topDivHeight);
   
   return (
     <div id='top-container' className="container">
