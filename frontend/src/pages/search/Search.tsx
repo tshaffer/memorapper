e665840bbe89d25d92586bd259c1728c33bf6ee1
Ted Shaffer <shaffer.family@gmail.com>
2024-12-06 09:47:48 -0800

    checkpoint - too much code

--- frontend/src/pages/search/Search.tsx
+++ frontend/src/pages/search/Search.tsx
@@ -11,17 +11,45 @@ const ResizableContainer: React.FC<ResizableContainerProps> = ({ initialTopDivHe
   const [topDivHeight, setTopDivHeight] = useState<string>(initialTopDivHeight);
   const handleRef = useRef<HTMLDivElement>(null);
 
-  const handleDrag = (event: DraggableEvent, ui: { y: number }) => {
-    const containerHeight: number = (event.target as HTMLElement).offsetHeight;
-    const sensitivityFactor = 0.5; // Adjust sensitivity as needed
+  const sensitivityFactor = 0.5; // Adjust sensitivity as needed
 
+  const handleDrag = (event: DraggableEvent, ui: { y: number }) => {
+
+    const containerId: string = (event.target as HTMLElement).id;
+    const containerHeight: number = (event.target as HTMLElement).offsetHeight;
+
+    console.log('container id and height:', containerId, containerHeight);
+    console.log('handle offset height', handleRef.current!.offsetHeight);
+    console.log('deltaY', ui.y);
+    // console.log('sensitivityFactor', sensitivityFactor);
+
+    const mathMax = Math.abs(ui.y * sensitivityFactor);
+    const secondPart = containerHeight - handleRef.current!.offsetHeight;
+    console.log('mathMax', mathMax);
+    console.log('secondPart', secondPart);
+    const denominator = containerHeight * 100;
+    console.log('denominator', denominator);
+
+    const numerator = Math.min(mathMax, secondPart);
+    console.log('mathMin', numerator);
+
+    const finalResult = numerator / denominator;
+    console.log('finalResult', finalResult);
+
+    // const newTopDivHeight = `${mathMin} / containerHeight * 100}%`;
     const newTopDivHeight = `${Math.min(Math.max(0, ui.y * sensitivityFactor), containerHeight - handleRef.current!.offsetHeight)} / containerHeight * 100}%`;
+    // console.log('newTopDivHeight', newTopDivHeight);
     setTopDivHeight(newTopDivHeight);
+
+    console.log('parseFloat(topDivHeight)', parseFloat(newTopDivHeight));
+
   };
 
+  console.log('topDivHeight', topDivHeight);
+  
   return (
-    <div className="container">
-      <div className="top-div" style={{ height: topDivHeight }} />
+    <div id='top-container' className="container">
+      <div id='top-div' className="top-div" style={{ height: topDivHeight }} />
       <Draggable
         axis="y"
         onDrag={handleDrag}
@@ -30,7 +58,7 @@ const ResizableContainer: React.FC<ResizableContainerProps> = ({ initialTopDivHe
       >
         <div ref={handleRef} className="handle">||</div>
       </Draggable>
-      <div className="bottom-div" style={{ height: `calc(100% - ${topDivHeight})` }} />
+      <div id='bottom-div' className="bottom-div" style={{ height: `calc(100% - ${topDivHeight})` }} />
     </div>
   );
 };
