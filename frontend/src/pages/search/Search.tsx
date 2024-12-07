81cee4078886b94e1aed3124c6dae0bd22d25bfa
Ted Shaffer <shaffer.family@gmail.com>
2024-12-06 16:29:15 -0800

    checkpoint

--- frontend/src/pages/search/Search.tsx
+++ frontend/src/pages/search/Search.tsx
@@ -4,22 +4,22 @@ import Draggable, { DraggableEvent } from 'react-draggable';
 import '../../styles/searchStyles.css';
 
 interface ResizableContainerProps {
-  initialTopDivHeight?: string;
+  initialTopDivHeight?: number;
 }
 
-const ResizableContainer: React.FC<ResizableContainerProps> = ({ initialTopDivHeight = '50%' }) => {
-  const [topDivHeight, setTopDivHeight] = useState<string>(initialTopDivHeight);
+const ResizableContainer: React.FC<ResizableContainerProps> = ({ initialTopDivHeight = 140 }) => {
+  const [topDivHeight, setTopDivHeight] = useState<number>(initialTopDivHeight);
   const handleRef = useRef<HTMLDivElement>(null);
 
-  const sensitivityFactor = 0.5; // Adjust sensitivity as needed
+  // const sensitivityFactor = 0.5; // Adjust sensitivity as needed
 
   const handleDrag = (event: DraggableEvent, ui: { y: number }) => {
 
-    const containerId: string = (event.target as HTMLElement).id;
-    const containerHeight: number = (event.target as HTMLElement).offsetHeight;
+    // const containerId: string = (event.target as HTMLElement).id;
+    // const containerHeight: number = (event.target as HTMLElement).offsetHeight;
 
-    console.log('container id and height:', containerId, containerHeight);
-    console.log('handle offset height', handleRef.current!.offsetHeight);
+    // console.log('container id and height:', containerId, containerHeight);
+    // console.log('handle offset height', handleRef.current!.offsetHeight);
     console.log('deltaY', ui.y);
     // console.log('sensitivityFactor', sensitivityFactor);
 
@@ -38,22 +38,30 @@ const ResizableContainer: React.FC<ResizableContainerProps> = ({ initialTopDivHe
 
     // const newTopDivHeight = `${Math.min(Math.max(0, ui.y * sensitivityFactor), containerHeight - handleRef.current!.offsetHeight)} / containerHeight * 100}%`;
     // setTopDivHeight(newTopDivHeight);
+    
+    const topContainerHeight = 300;
+    const handleHeight = handleRef.current!.offsetHeight;
+    const newTopDivHeight = (topContainerHeight / 2 - handleHeight / 2) + ui.y ;
+    setTopDivHeight(newTopDivHeight);
   };
 
-  // console.log('topDivHeight', topDivHeight);
-  
+  console.log('topDivHeight', topDivHeight);
+  console.log(topDivHeight.toString() + 'px');
+  console.log((300 - 18 - topDivHeight).toString() + 'px')
+
   return (
     <div id='top-container' className="container">
-      <div id='top-div' className="top-div" style={{ height: topDivHeight }} />
+      {/* <div id='top-div' className="top-div" style={{ height: topDivHeight.toString() + '%' }} /> */}
+      <div id='top-div' className="top-div" style={{ height: topDivHeight.toString() + 'px' }} />
       <Draggable
         axis="y"
         onDrag={handleDrag}
         handle=".handle"
-        // position={{ x: 0, y: parseFloat(topDivHeight) }} // Adjust position based on topDivHeight
+        // position={{ x: 0, y: 0 }} // Adjust position based on topDivHeight
       >
         <div ref={handleRef} className="handle">||</div>
       </Draggable>
-      <div id='bottom-div' className="bottom-div" style={{ height: `calc(100% - ${topDivHeight})` }} />
+      <div id='bottom-div' className="bottom-div" style={{ height: (300-18-topDivHeight).toString() + 'px' }} />
     </div>
   );
 };
