187f92ae42e7a0d2898a39d9fc06169d055e9d5a
Ted Shaffer <shaffer.family@gmail.com>
2024-12-12 15:46:24 -0800

    drag events

--- frontend/src/pages/dndTouch/DndTouchDraggable.tsx
+++ frontend/src/pages/dndTouch/DndTouchDraggable.tsx
@@ -1,4 +1,12 @@
 import {
+  DragAbortEvent,
+  DragCancelEvent,
+  DragEndEvent,
+  DragMoveEvent,
+  DragOverEvent,
+  DragPendingEvent,
+  DragStartEvent,
+  useDndMonitor,
   useDraggable} from '@dnd-kit/core'
 import './dnd.css'
 
@@ -8,6 +16,24 @@ interface DraggableProps {
 
 const Draggable = function Draggable({ id }: DraggableProps) {
   const { setNodeRef, listeners, attributes, transform } = useDraggable({ id })
+
+  useDndMonitor({
+    onDragAbort(event: DragAbortEvent) { console.log('onDragAbort', event) },
+    onDragPending(event: DragPendingEvent) { console.log('onDragPending', event) },
+    onDragStart(event: DragStartEvent) { console.log('onDragStart', event) },
+    onDragMove(event: DragMoveEvent) { console.log('onDragMove', event) },
+    onDragOver(event: DragOverEvent) { console.log('onDragOver', event) },
+    onDragEnd(event: DragEndEvent) { console.log('onDragEnd', event) },
+    onDragCancel(event: DragCancelEvent) { console.log('onDragCancel', event) },
+  });
+  
+  // onDragAbort?(event: DragAbortEvent): void;
+  // onDragPending?(event: DragPendingEvent): void;
+  // onDragStart?(event: DragStartEvent): void;
+  // onDragMove?(event: DragMoveEvent): void;
+  // onDragOver?(event: DragOverEvent): void;
+  // onDragEnd?(event: DragEndEvent): void;
+  // onDragCancel?(event: DragCancelEvent): void;
 
   const style = transform
       ? {
