8005902be66e362662b536bcb341225404227628
Ted Shaffer <shaffer.family@gmail.com>
2024-12-13 05:47:07 -0800

    cp

--- frontend/src/pages/dndTouch/DndTouchDraggable.tsx
+++ frontend/src/pages/dndTouch/DndTouchDraggable.tsx
@@ -14,44 +14,68 @@ interface DraggableProps {
   id: string
 }
 
-const Draggable = function Draggable({ id }: DraggableProps) {
-  const { setNodeRef, listeners, attributes, transform } = useDraggable({ id })
+// const Draggable = function Draggable({ id }: DraggableProps) {
+//   const { setNodeRef, listeners, attributes, transform } = useDraggable({ id })
 
-  useDndMonitor({
-    onDragAbort(event: DragAbortEvent) { console.log('onDragAbort', event) },
-    onDragPending(event: DragPendingEvent) { console.log('onDragPending', event) },
-    onDragStart(event: DragStartEvent) { console.log('onDragStart', event) },
-    onDragMove(event: DragMoveEvent) { console.log('onDragMove', event) },
-    onDragOver(event: DragOverEvent) { console.log('onDragOver', event) },
-    onDragEnd(event: DragEndEvent) { console.log('onDragEnd', event) },
-    onDragCancel(event: DragCancelEvent) { console.log('onDragCancel', event) },
-  });
+//   useDndMonitor({
+//     onDragAbort(event: DragAbortEvent) { console.log('onDragAbort', event) },
+//     onDragPending(event: DragPendingEvent) { console.log('onDragPending', event) },
+//     onDragStart(event: DragStartEvent) { console.log('onDragStart', event) },
+//     onDragMove(event: DragMoveEvent) { console.log('onDragMove', event) },
+//     onDragOver(event: DragOverEvent) { console.log('onDragOver', event) },
+//     onDragEnd(event: DragEndEvent) { console.log('onDragEnd', event) },
+//     onDragCancel(event: DragCancelEvent) { console.log('onDragCancel', event) },
+//   });
   
-  // onDragAbort?(event: DragAbortEvent): void;
-  // onDragPending?(event: DragPendingEvent): void;
-  // onDragStart?(event: DragStartEvent): void;
-  // onDragMove?(event: DragMoveEvent): void;
-  // onDragOver?(event: DragOverEvent): void;
-  // onDragEnd?(event: DragEndEvent): void;
-  // onDragCancel?(event: DragCancelEvent): void;
+//   const style = transform
+//       ? {
+//             transform: `translate(${transform.x}px, ${transform.y}px)`
+//         }
+//       : undefined
 
-  const style = transform
-      ? {
-            transform: `translate(${transform.x}px, ${transform.y}px)`
-        }
-      : undefined
+//   return (
+//       <div
+//           className="draggable"
+//           ref={setNodeRef}
+//           style={style}
+//           {...listeners}
+//           {...attributes}
+//       >
+//           {/* Draggable {id} */}
+//       </div>
+//   )
+// }
+
+const Draggable: React.FC = () => {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: 'draggable-item',
+  });
+
+  const positionY = 100;
+  // Apply vertical-only transformation
+  const style: React.CSSProperties = {
+    transform: transform ? `translateY(${transform.y}px)` : undefined,
+    position: 'absolute',
+    left: '50%',
+    top: `${positionY}px`,
+    width: '200px',
+    height: '50px',
+    backgroundColor: 'skyblue',
+    cursor: 'row-resize',
+    textAlign: 'center',
+    lineHeight: '50px',
+  };
 
   return (
-      <div
-          className="draggable"
-          ref={setNodeRef}
-          style={style}
-          {...listeners}
-          {...attributes}
-      >
-          {/* Draggable {id} */}
-      </div>
-  )
-}
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...listeners}
+      {...attributes}
+    >
+      Drag Me Vertically
+    </div>
+  );
+};
 
 export default Draggable;
