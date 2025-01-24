96ac56388c4a7f9eb0a5a83ed0dea1107c0f8d34
Ted Shaffer <shaffer.family@gmail.com>
2025-01-24 08:35:55 -0700

    centralize data retrieval in userContext

--- frontend/src/contexts/UserContext.tsx
+++ frontend/src/contexts/UserContext.tsx
@@ -1,8 +1,24 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { DiningGroup, DistanceAwayFilterValues, Filters, Settings } from '../types';
+import { Diner, DiningGroup, DistanceAwayFilterValues, Filters, GooglePlace, NewRestaurant, ReviewedRestaurant, Settings, VisitReview } from '../types';
 
 interface UserContextValue {
   diningGroups: DiningGroup[];
+
+  diners: Diner[];
+  setDiners: (diners: Diner[]) => void;
+
+  places: GooglePlace[];
+  setPlaces: (places: GooglePlace[]) => void;
+
+  reviewedRestaurants: ReviewedRestaurant[];
+  setReviewedRestaurants: (reviewedRestaurants: ReviewedRestaurant[]) => void;
+
+  newRestaurants: NewRestaurant[];
+  setNewRestaurants: (newRestaurants: NewRestaurant[]) => void;
+
+  reviews: VisitReview[];
+  setReviews: (reviews: VisitReview[]) => void;
+
   currentDiningGroup: DiningGroup | null;
   setCurrentDiningGroup: (diningGroup: DiningGroup | null) => void;
 
@@ -17,6 +33,12 @@ const UserContext = createContext<UserContextValue | undefined>(undefined);
 
 export const UserProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [diningGroups, setDiningGroups] = useState<DiningGroup[]>([]);
+  const [diners, setDiners] = useState<Diner[]>([]);
+  const [places, setPlaces] = useState<GooglePlace[]>([]);
+  const [reviewedRestaurants, setReviewedRestaurants] = useState<ReviewedRestaurant[]>([]);
+  const [newRestaurants, setNewRestaurants] = useState<NewRestaurant[]>([]);
+  const [reviews, setReviews] = useState<VisitReview[]>([]);
+
   const [currentDiningGroup, setCurrentDiningGroup] = useState<DiningGroup | null>(null);
 
   const [settings, setSettingsState] = useState<Settings>({
@@ -41,30 +63,70 @@ export const UserProvider: React.FC<{ children: React.ReactNode }> = ({ children
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
+
     const fetchDiningGroups = async () => {
-      try {
-        setLoading(true);
-        const response = await fetch('/api/diningGroups');
-        if (!response.ok) {
-          throw new Error('Failed to fetch dining groups');
-        }
-        const data = await response.json();
-        setDiningGroups(data.diningGroups as DiningGroup[]);
-        setError(null);
-      } catch (err: any) {
-        setError(err.message || 'Something went wrong');
-      } finally {
-        setLoading(false);
-      }
+      const response = await fetch('/api/diningGroups');
+      const data = await response.json();
+      setDiningGroups(data.diningGroups as DiningGroup[]);
     };
 
-    fetchDiningGroups();
+    const fetchDiners = async () => {
+      const response = await fetch('/api/diners');
+      const data = await response.json();
+      setDiners(data.diners);
+    }
+
+    const fetchPlaces = async () => {
+      const response = await fetch('/api/places');
+      const data = await response.json();
+      setPlaces(data.googlePlaces);
+    };
+
+    const fetchReviewedRestaurants = async () => {
+      const response = await fetch('/api/reviewedRestaurants');
+      const data = await response.json();
+      setReviewedRestaurants(data.reviewedRestaurants);
+    };
+
+    const fetchNewRestaurants = async () => {
+      const response = await fetch('/api/newRestaurants');
+      const data = await response.json();
+      setNewRestaurants(data.newRestaurants);
+    };
+
+    const fetchReviews = async () => {
+      const response = await fetch('/api/visitReviews');
+      const data = await response.json();
+      setReviews(data.visitReviews);
+    };
+
+    const fetchData = async () => {
+      await fetchDiningGroups();
+      await fetchDiners();
+      await fetchPlaces();
+      await fetchReviewedRestaurants();
+      await fetchReviews();
+      setLoading(false);
+    };
+
+    fetchData();
+
   }, []);
 
   return (
     <UserContext.Provider
       value={{
         diningGroups: diningGroups,
+        diners: diners,
+        setDiners: setDiners,
+        places: places,
+        setPlaces: setPlaces,
+        reviewedRestaurants: reviewedRestaurants,
+        setReviewedRestaurants: setReviewedRestaurants,
+        newRestaurants: newRestaurants,
+        setNewRestaurants: setNewRestaurants,
+        reviews: reviews,
+        setReviews: setReviews,
         currentDiningGroup: currentDiningGroup,
         settings,
         setCurrentDiningGroup: setCurrentDiningGroup,
