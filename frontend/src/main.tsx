6dee27ac7e1ebed70c53be8dacfdc2467eaafc5b
Ted Shaffer <shaffer.family@gmail.com>
2025-01-27 05:58:23 -0700

    remove strict mode to avoid double calls

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,11 +6,9 @@ import { UserProvider } from './contexts/UserContext';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <UserProvider>
-      <Router>
-        <App />
-      </Router>
-    </UserProvider>
-  </StrictMode>,
+  <UserProvider>
+    <Router>
+      <App />
+    </Router>
+  </UserProvider>
 )
