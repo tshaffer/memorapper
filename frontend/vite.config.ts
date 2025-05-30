dbfbb3b3a3a2acc44026e20bb7dc2404bfee3923
Ted Shaffer <shaffer.family@gmail.com>
2025-05-30 06:11:32 -0700

    port 5001; 5000 stopped working.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://localhost:5000', // Backend URL
+                target: 'http://localhost:5001', // Backend URL
                 changeOrigin: true,
                 secure: false,
             },
