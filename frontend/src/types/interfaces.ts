f955ffc1a08a5c8b02e5cb5b5e19890076d565c8
Ted Shaffer <shaffer.family@gmail.com>
2025-06-03 13:54:02 -0700

    more trimming

--- frontend/src/types/interfaces.ts
+++ frontend/src/types/interfaces.ts
@@ -1,6 +1,4 @@
-import { ItemReview } from "./entities";
 import { Distance, RestaurantOpen, PlaceType, RestaurantType } from "./enums";
-import { GooglePlace } from "./place";
 
 export interface Filters {
   distanceAway: Distance;
@@ -29,18 +27,5 @@ export interface OpenForMeals {
   Breakfast: boolean;
   Lunch: boolean;
   Dinner: boolean;
-}
-
-export interface ReviewData {
-  _id: string;
-  // diningGroupid: string;
-  place: GooglePlace | null;
-  // dinerRestaurantReviews: DinerRestaurantReview[];
-  dateOfVisit: string | null;
-  reviewText: string | null;
-  itemReviews: ItemReview[];
-  sessionId: string | null;
-  restaurantName: string;
-  // chatHistory: ChatMessage[]; // Chat history
 }
 
