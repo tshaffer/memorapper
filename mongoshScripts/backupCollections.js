debef6e276570b4e986ba3c640f5be8c6ad2bf89
Ted Shaffer <shaffer.family@gmail.com>
2025-06-15 04:49:42 -0700

    script update

--- mongoshScripts/backupCollections.js
+++ mongoshScripts/backupCollections.js
@@ -5,7 +5,7 @@ const collections = [
   "mrplaces",
 ];
 
-const outputDir = "/Users/tedshaffer/Documents/MongoDBBackups/memoRapper/backup-06-11-03";
+const outputDir = "/Users/tedshaffer/Documents/MongoDBBackups/memoRapper/backup-06-14-01";
 
 const fs = require("fs");
 
